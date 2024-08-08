2bee21611408a43b035bac944ff33f132ffefc64
Debjit25 <debjitmaji123@gmail.com>
2024-08-09 00:54:59 +0530

    Admin add feature

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Orders from "./pages/Orders/Orders";
+import List from "./pages/List/List";
+import Add from "./pages/Add/Add";
+
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
